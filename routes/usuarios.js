a82d8fde3c685860d70ee084854b839f85115ab0
kikemaya <enrique.mg.cd@gmail.com>
2022-06-08 17:15:20 -0500

    :sparkles: crud usuarios, lògica de peticiones, esperando validaciones...

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,19 +4,10 @@ const router = express.Router()
 
 const Usuario = require("./../models/usuario_model")
 
-router.get("/", (req, res) => {
-    res.json("Listo el get de usuarios")
-})
-
-router.post("/", (req, res) => {
-    let body = req.body
-    let resultado = crearUsuario(body)
-
-    resultado
-        .then(user => res.status(201).json({ value: user }))
-        .catch(err => res.status(400).json({ error: err }))
-})
-
+async function listarUsuariosActivos() {
+    let usuarios = await Usuario.find({ estado: true })
+    return usuarios
+}
 async function crearUsuario(body) {
     let usuario = new Usuario({
         email: body.email,
@@ -26,5 +17,54 @@ async function crearUsuario(body) {
 
     return await usuario.save()
 }
+async function actualizarUsuario(email, body) {
+    let usuario = await Usuario.findOneAndUpdate({ email: email }, {
+        $set: {
+            nombre: body.nombre,
+            password: body.password
+        }
+    }, { new: true })
+
+    return usuario
+}
+async function desactivarUsuario(email) {
+    let usuario = await Usuario.findOneAndUpdate({ email: email }, {
+        $set: {
+            estado: false
+        }
+    }, { new: true })
+
+    return usuario
+}
+
+router.get("/", (req, res) => {
+    let resultado = listarUsuariosActivos()
+
+    resultado
+        .then(users => res.status(200).json({ users }))
+        .catch(err => res.status(400).json({ error: err }))
+})
+router.post("/", (req, res) => {
+    let body = req.body
+    let resultado = crearUsuario(body)
+
+    resultado
+        .then(user => res.status(201).json({ value: user }))
+        .catch(err => res.status(400).json({ error: err }))
+})
+router.put("/:email", (req, res) => {
+    let resultado = actualizarUsuario(req.params.email, req.body)
+
+    resultado
+        .then(valor => res.status(201).json({ valor: valor }))
+        .catch(err => res.status(400).json({ error: err }))
+})
+router.delete("/:email", (req, res) => {
+    let resultado = desactivarUsuario(req.params.email)
+
+    resultado
+        .then(valor => res.json({ usuario: valor }))
+        .catch(err => res.status(400).json({ error: err }))
+})
 
 module.exports = router
